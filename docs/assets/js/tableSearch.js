02895c7e736491bbea6a134ba9b2a2c256607876
Alejandro León <60055243+fal3on@users.noreply.github.com>
2021-04-22 17:55:12 -0700

    Delete docs directory
    Docs folder removal

--- docs/assets/js/tableSearch.js
+++
@@ -1,6 +0,0 @@
-function myFunction(){
-    const filter = document.querySelector('#myInput').value.toUpperCase();
-    const trs = document.querySelectorAll('#myTable tr:not(.header)');
-    trs.forEach(tr => tr.style.display = [...tr.children].find(td => td.innerHTML.toUpperCase().includes(filter)) ? '' : 'none');
-
-  }
